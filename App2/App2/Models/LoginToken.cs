3c064aef68667167492b36bc3e330b8f8eda8f9c
annv3 <annv3@TS0813423-PM-I5>
2022-08-11 09:49:25 +0700

    change position of logo

---
+++ App2/App2/Models/LoginToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Models
+{
+    public class LoginToken
+    {
+        public string Token { get; set; }
+        public string UserName { get; set; }
+        public string Validaty { get; set; }
+        public object RefreshToken { get; set; }
+        public string Id { get; set; }
+        public object EmailId { get; set; }
+        public string GuidId { get; set; }
+        public DateTime ExpiredTime { get; set; }
+    }
+}
