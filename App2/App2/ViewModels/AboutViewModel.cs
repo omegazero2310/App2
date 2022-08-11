501bd0e68b7f652fd3cc5eb24996de3996980e6e
annv3 <annv3@TS0813423-PM-I5>
2022-08-11 13:40:09 +0700

    design: add UserProfile UI

--- App2/App2/ViewModels/AboutViewModel.cs
+++ App2/App2/ViewModels/AboutViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App2.Views;
+using System;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -11,8 +12,25 @@ namespace App2.ViewModels
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Shell.Current.GoToAsync("///ItemsPage"));
+            _imageProfile = ImageSource.FromResource("App2.Imgs.BingWallpaper.jpg", typeof(LoginPage).Assembly);
         }
 
         public ICommand OpenWebCommand { get; }
+        private ImageSource _imageProfile;
+        public ImageSource ImageProfile
+        {
+            get 
+            { 
+                if( _imageProfile == null )
+                    _imageProfile = ImageSource.FromResource("App2.Imgs.BingWallpaper.jpg", typeof(LoginPage).Assembly);
+                return _imageProfile; 
+            }
+            set 
+            { 
+                _imageProfile = value;
+                OnPropertyChanged("ImageProfile"); 
+            }
+        }
+
     }
 }
